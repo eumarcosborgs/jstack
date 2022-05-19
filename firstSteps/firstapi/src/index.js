411e31cd84aa14f72460247175ba994c3c2c5348
Nome Sobrenome <eumarcosborgs@gmail.com>
2022-05-18 22:07:26 -0300

    creating first API route

---
+++ firstSteps/firstapi/src/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const users = require('./mocks/users.js');
+
+const server = http.createServer((request, response) => {
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(200, { 'Content-Type': 'text/html'});
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+
+  // response.writeHead(200, { 'Content-Type': 'text/html'});
+  // response.end('<h1>Hello World</h1>');
+})
+
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+})
